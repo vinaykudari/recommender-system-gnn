ac14948a33d5471d0fa1649607b0140535d6d644
Ujwal <ujwal98@gmail.com>
2022-05-22 16:24:03 -0400

    made code modular

--- models/igmc.py
+++ models/igmc.py
@@ -10,10 +10,11 @@ class IGMC(torch.nn.Module):
             self, dataset, gconv=RGCNConv,
             latent_dim=[32, 32, 32, 32], num_relations=5,
             num_bases=4, side_features=False,
-            n_side_features=0, adj_dropout=0.2,
+            n_side_features=0, adj_dropout=0.2,rnn=False, rnn_layers = 2
     ):
         super(IGMC, self).__init__()
         self.adj_dropout = adj_dropout
+        self.rnn = rnn
         self.convs = torch.nn.ModuleList()
         self.convs.append(
             gconv(
@@ -30,7 +31,7 @@ class IGMC(torch.nn.Module):
             )
 
         # self.emb = Embedding(90524,3233)
-        self.rnn1 = LSTM(2 * sum(latent_dim),2 * sum(latent_dim),num_layers=2)
+        self.rnn1 = LSTM(2 * sum(latent_dim),2 * sum(latent_dim),num_layers=rnn_layers)
         self.lin1 = Linear(2 * sum(latent_dim), 128)
         self.side_features = side_features
         if side_features:
@@ -48,9 +49,6 @@ class IGMC(torch.nn.Module):
 
     def forward(self, data):
         x, edge_index, edge_type, batch = data.x, data.edge_index, data.edge_type, data.batch
-        # print(x)
-        # print(x.shape)
-        # break
         if self.adj_dropout > 0:
             edge_index, edge_type = dropout_adj(
                 edge_index,
@@ -59,15 +57,7 @@ class IGMC(torch.nn.Module):
                 num_nodes=len(x),
                 training=self.training,
             )
-        # print(batch)
-        # print(sum(latent_dim))
-        # hx = torch.randn(2, batch, 2 * sum(latent_dim))
-        # cx = torch.randn(2, batch, 2 * sum(latent_dim))
         concat_states = []
-        # print(x.shape)
-        # print(edge_index.shape)
-        # print(edge_type.shape)
-        # x = self.emb(x.long())
         for conv in self.convs:
             x = torch.tanh(conv(x, edge_index, edge_type))
             concat_states.append(x)
@@ -80,10 +70,8 @@ class IGMC(torch.nn.Module):
 
         if self.side_features:
             x = torch.cat([x, data.u_feature, data.v_feature], 1)
-        # print(x.shape)
-        x,hidden = self.rnn1(x)
-        # print(x.shape)
-        # print(hidden.shape)
+        if self.rnn:
+            x, _ = self.rnn1(x)
         x = F.relu(self.lin1(x))
         x = F.dropout(x, p=self.adj_dropout, training=self.training)
         x = self.lin2(x)
